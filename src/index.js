2480fbb9a8489ddd62a5f2ec29e972400dad01b7
HassanKhan123 <hk2602809@gmail.com>
2020-10-14 23:51:52 +0500

    first graphql api created using graphql-yoga

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('Hello GraphQL')
+import { GraphQLServer } from 'graphql-yoga';
+
+//Type Definitions (Schemas)
+const typeDefs = `
+    type Query {
+        hello: String!
+        name:  String!
+        location:  String!
+        bio:  String!
+    }
+
+`;
+
+//Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return 'First Query';
+    },
+    name(){
+        return 'Hassan'
+    },
+    location(){
+        return 'Karachi, Pakistan'
+    },
+    bio(){
+        return 'I am MERN Developer'
+    }
+  },
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start(() => console.log('Server is running'));
