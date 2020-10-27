0090ae8376a1385beb7ebd4aee424484eb21ada7
HassanKhan123 <hk2602809@gmail.com>
2020-10-27 07:17:27 +0500

    array with custom types

--- src/index.js
+++ src/index.js
@@ -3,6 +3,38 @@ import { GraphQLServer } from 'graphql-yoga';
 // Types
 // Scalar Types - Strings, Boolean, Int, Float, ID
 
+const users = [
+  {
+    id: 'abc124',
+    name: 'Hassan',
+    age: 21,
+  },
+  {
+    id: 'abc123',
+    name: 'Khan',
+    age: 22,
+  },
+];
+
+const posts = [
+  {
+    id: '12az',
+    title: 'GraphQL',
+    body: 'Best Place to learn graphQL',
+    published: true,
+  },{
+    id: '12aa',
+    title: 'Node',
+    body: 'Great Place to learn Node',
+    published: false,
+  },{
+    id: '12a1',
+    title: 'React',
+    body: 'Awesome Place to learn React',
+    published: true,
+  }
+]
+
 //Type Definitions (Schemas)
 const typeDefs = `
     type Query {
@@ -12,6 +44,8 @@ const typeDefs = `
       me:User!
       post:Post!
       grades:[Int!]!
+      users(query:String):[User!]!
+      posts(query:String):[Post!]!
     }
 
     type User {
@@ -32,39 +66,54 @@ const typeDefs = `
 //Resolvers
 const resolvers = {
   Query: {
-    greeting(parent,args,ctx,info){
-      if(args.name) return `Hello, ${args.name}!`
-      return `Hello!`
+    greeting(parent, args, ctx, info) {
+      if (args.name) return `Hello, ${args.name}!`;
+      return `Hello!`;
     },
-    add(parent,args,ctx,info){
-      let sum = args.num1 + args.num2
-      return sum
+    add(parent, args, ctx, info) {
+      let sum = args.num1 + args.num2;
+      return sum;
     },
-    sum(parent,args,ctx,info){
-      if(args.numbers.length === 0) return 0
-      return args.numbers.reduce((acc,cur) => {
-        return acc+cur
-      })
+    sum(parent, args, ctx, info) {
+      if (args.numbers.length === 0) return 0;
+      return args.numbers.reduce((acc, cur) => {
+        return acc + cur;
+      });
     },
-    grades(parent,args,ctx,info){
-      return [1,2,3]
+    grades(parent, args, ctx, info) {
+      return [1, 2, 3];
     },
-    me(){
+    me() {
       return {
-        id:'abc123',
-        name:'Hassan',
-       age:21
-      }
+        id: 'abc123',
+        name: 'Hassan',
+        age: 21,
+      };
     },
-    post(){
-      return{
-        id:'12az',
-        title:'GraphQL',
-        body:"Best Place to learn graphQL",
-        published:true
-      }
+    post() {
+      return {
+        id: '12az',
+        title: 'GraphQL',
+        body: 'Best Place to learn graphQL',
+        published: true,
+      };
+    },
+    users(parent, args, ctx, info) {
+      if(!args.query) return users
+      return users.filter(user => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase())
+      })
+
+    },
+    posts(parent, args, ctx, info){
+      if(!args.query) return posts
+      return posts.filter(post => {
+        const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+        const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+        return isTitleMatch || isBodyMatch 
+      })
     }
-    
+
   },
 };
 
